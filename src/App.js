9e96b7900d45f55189cc75374423a9e5fabe39ee
Caio Christhian <caiochristhianz@gmail.com>
2022-05-03 11:14:28 -0300

    defininfo tipos para as props

--- src/App.js
+++ src/App.js
@@ -10,22 +10,25 @@ function App() {
       <Header title="Kuroishi's">
       <h2>Posts da semana</h2>
       </Header>
-    
+
       <hr />
 
       <Post 
+        likes={20}
         post={{
           title: 'Title 01',
           subtitle: 'Subtitle 01'
         }}
       />
       <Post 
+        likes={10}
         post={{
           title: 'Title 02',
           subtitle: 'Subtitle 02'
         }}
       />
       <Post 
+        likes={50}
         post={{
           title: 'Title 03',
           subtitle: 'Subtitle 03'
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,6 @@
-import React from "react"
+import React from "react";
+import PropTypes from "prop-types";
+
 
 export default function Header(props) {
   return (
@@ -7,4 +9,13 @@ export default function Header(props) {
       {props.children} {/* recebe tudo que colocar dentro da tag/componente */}
     </>
   )
-}
+};
+
+Header.PropTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node, // node pode receber uma string, componente, tag html, qualquer elemento
+};
+
+Header.defaultProps = {
+  title: `Kuroishi's Blog`
+};
