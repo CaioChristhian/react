fdb6eeaf6962092ad336a55db731cb3aba964334
Caio Christhian <caiochristhianz@gmail.com>
2022-05-18 17:29:25 -0300

    change theme com prop drilling

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import Header from './Header';
 
 
 function App() {
+  const [theme, setTheme] = useState('dark');
+
   const [posts, setPosts] = useState([
     { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false },
     { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true },
     { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false },
     { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 100, read: false },
   ])
+
+  function handleToggleTheme() {
+    setTheme((prevState) => (
+      prevState === 'dark' ? 'light' : 'dark'
+    ))
+  }
 
   function handleRefresh() {
     setPosts((prevState) => [
@@ -23,7 +31,6 @@ function App() {
       }
     ])
   }
-
   
   function handleRemovePost(postId) {
     setPosts((prevState) => (
@@ -33,7 +40,7 @@ function App() {
 
   return (
     <>
-      <Header title="Kuroishi's">
+      <Header theme={theme} title="Kuroishi's" onToggleTheme={handleToggleTheme}>
         <h2>
           Posts da semana
           <button onClick={handleRefresh}>
@@ -49,6 +56,7 @@ function App() {
           onRemove={handleRemovePost}
           key={post.id}
           post={post}
+          theme={theme}
         />
       ))}
     </>
--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 
 export default function Header(props) {
   return (
     <>
       <h1>{props.title}</h1>
+      <Button theme={props.theme} onClick={props.onToggleTheme}>
+        Mudar tema
+      </Button>
       {props.children} {/* recebe tudo que colocar dentro da tag/componente */}
     </>
   )
 };
 
 Header.PropTypes = {
+  theme: PropTypes.string.isRequired,
+  onToggleTheme: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired,
   children: PropTypes.node, // node pode receber uma string, componente, tag html, qualquer elemento
 };
