da1b8fcf9ed33f3a4c25a5586f99d3fa12e08458
Caio Christhian <caiochristhianz@gmail.com>
2022-05-30 10:48:34 -0300

    difinindo varios estilos dentro da mesma função

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,10 +1,12 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.article`
   margin-bottom: 24px;
 
-  opacity: ${(props) => props.removed ? 0.5 : 1};
-  color: ${(props) => props.removed ? '#f00' : '#fff'};
+  ${(props) => css`
+    opacity: ${(props) => props.removed ? 0.5 : 1};
+    color: ${(props) => props.removed ? '#f00' : '#fff'};
+  `}
 `;
 
 export const Subtitle = styled.small`
