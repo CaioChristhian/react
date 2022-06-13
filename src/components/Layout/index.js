27535239b49f3c7a158b12998755f1438fab051d
Caio Christhian <caiochristhianz@gmail.com>
2022-06-13 17:02:33 -0300

    criando as primeiras rotas

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import Header from '../Header';
-import PostsList from '../PostList';
 import Footer from '../Footer';
+
+import Routes from '../../routes';
 
 export default function Layout() {
   return (
     <>
       <Header />
-      <PostsList />
+      <Routes />
       <Footer />
     </>
   );
