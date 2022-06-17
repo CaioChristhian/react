c6f38a1c1d11cbcc20ec1d984bd81c57d2630f38
Caio Christhian <caiochristhianz@gmail.com>
2022-06-17 14:44:35 -0300

    prop exact e component link, nav bar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Link, BrowserRouter } from 'react-router-dom';
 
 import Header from '../Header';
 import Footer from '../Footer';
-
 import Routes from '../../routes';
+import { Nav } from './styles';
 
 export default function Layout() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
+      <Nav>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Posts</Link>
+      </Nav>
       <Routes />
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
---
+++ src/components/Layout/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  background: #000;
+  margin-top: 16px;
+  padding: 16px;
+  border-radius: 4px;
+
+  a {
+    color: #fff;
+    text-decoration: none;
+    display: inline-block;
+    
+   
+
+    & + a {
+      margin-left: 16px;
+    }
+  }
+  
+  a:hover {
+    color: #6d6d6d;
+    transition: 0.3s;
+  }
+`
